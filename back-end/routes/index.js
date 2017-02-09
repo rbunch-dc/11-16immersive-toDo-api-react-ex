7a4d37bfe6441231588c6a6b691e38992e5eac03
Robert Bunch <rob@digitalcrafts.com>
2017-02-09 16:59:45 -0500

    Add ajax

---
+++ back-end/routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'x',
+	password: 'x',
+	database: 'students'
+});
+connection.connect();
+
+
+router.get('/getStudents', (req,res,next)=>{
+	connection.query('SELECT * FROM students', (error, results, fields)=>{
+		if(error) throw error;
+		res.json(results);
+	});
+});
+
+router.post('/addStudent', (req, res, next)=>{
+	var studentToAdd = req.body.name;
+	// console.log(req.body);
+	connection.query('INSERT INTO students (name) VALUES (?)', [studentToAdd], (error, results, fields)=>{
+		if (error) throw error;
+		connection.query('SELECT * FROM students', (error, results, fields)=>{
+			if(error) throw error;
+			res.json(results);
+		});		
+	})
+});
+
+module.exports = router;
